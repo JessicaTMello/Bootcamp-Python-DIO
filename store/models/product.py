5c86a23a2c4b913d4bfb2082aff48752ef04063c
jessicatmello <jessica_thaize@hotmail.com>
2024-07-14 20:22:55 -0300

    atualização

--- store/models/product.py
+++
@@ -1,6 +0,0 @@
-from store.models.base import CreateBaseModel
-from store.schemas.product import ProductIn
-
-
-class ProductModel(ProductIn, CreateBaseModel):
-    ...
--- store/routers.py
+++
@@ -1,5 +0,0 @@
-from fastapi import APIRouter
-from store.controllers.product import router as product
-
-api_router = APIRouter()
-api_router.include_router(product, prefix="/products")
